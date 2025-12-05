cd0e4356d9c3d13cad800ef1ddb386f013cf78e2
= <=>
2025-12-05 19:07:45 +0200

    Added queue

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Parser import *
 from Stack import *
+from Queue import *
 def main():
-
-
-
+    while True:
+        expression = input("Enter expression: ")
+        print(evaluate(expression))
 main()
